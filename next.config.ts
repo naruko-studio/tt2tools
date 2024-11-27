7c18328f44603559d2197902e8abf0cb7de3016d
naruko_hstk <projectcsj@gmail.com>
2024-11-27 18:30:04 +0800

    fix: update basePath
    設定basePath

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:"export",
-  trailingSlash:true,
-  distDir:"dist",
-  images:{
-    unoptimized:true,
-  }
+  output: "export",
+  basePath: "/tt2tools",
+  trailingSlash: true,
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
